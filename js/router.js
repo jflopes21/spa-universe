f794dc8fa2ec86629db88451819cb95c859f87ca
Joao Francisco <joaofrancisco21@yahoo.com.br>
2023-07-31 13:28:18 -0300

    First commit

---
+++ js/router.js
@@ -0,0 +1,44 @@
+export class Router {
+  routes = {};
+  app = document.querySelector("#app");
+
+  add(routeName, page) {
+    this.routes[routeName] = page;
+  }
+
+  route(event) {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+
+    const links = document.querySelectorAll("nav a");
+    links.forEach((link) => {
+      link.classList.remove("a-active");
+    });
+
+    const clickedLink = event.target;
+    clickedLink.classList.add("a-active");
+
+    this.handle();
+  }
+
+  handle() {
+    const { pathname } = window.location;
+    const route = this.routes[pathname] || this.routes[404];
+    fetch(route)
+      .then((data) => data.text())
+      .then((html) => {
+        app.innerHTML = html;
+        if (
+          route == "/pages/universe.html" ||
+          route == "/pages/exploration.html"
+        ) {
+          app.classList.remove("app-home");
+          app.classList.add("app-default");
+        } else {
+          app.classList.remove("app-default");
+          app.classList.add("app-home");
+        }
+      });
+  }
+}
